81698bed22cab3215f94c317403df2b15fd2facc
milanverma2005 <milan.24mim10076@vitbhopal.ac.in>
2026-03-31 18:02:40 +0530

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -10,11 +10,9 @@ for dir in "${directories[@]}"
 do
     echo "Directory: $dir"
 
-    # Disk usage
     usage=$(du -sh $dir 2>/dev/null | cut -f1)
     echo "Disk Usage: $usage"
 
-    # Owner and permissions
     details=$(ls -ld $dir | awk '{print $1, $3, $4}')
     echo "Permissions & Owner: $details"
 
