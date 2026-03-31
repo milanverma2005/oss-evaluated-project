8afa452c926abec5b9796843f9b486368d8a5919
milanverma2005 <milan.24mim10076@vitbhopal.ac.in>
2026-03-31 18:01:43 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -4,27 +4,18 @@ echo "=============================="
 echo "   SYSTEM IDENTITY REPORT"
 echo "=============================="
 
-# Linux Distribution and Kernel
-distro=$(cat /etc/os-release | grep "PRETTY_NAME" | cut -d= -f2 | tr -d '"')
+distro=$(grep PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d '"')
 kernel=$(uname -r)
-
-# User and Home Directory
 user=$(whoami)
 home_dir=$HOME
-
-# Uptime and Date
 uptime=$(uptime -p)
 current_time=$(date)
 
-# Output
 echo "Operating System: $distro"
 echo "Kernel Version: $kernel"
 echo "Logged-in User: $user"
 echo "Home Directory: $home_dir"
 echo "System Uptime: $uptime"
 echo "Current Date & Time: $current_time"
-
-# License Info
-echo "License: Most Linux distributions are based on the GNU General Public License (GPL)."
 
 echo "=============================="
